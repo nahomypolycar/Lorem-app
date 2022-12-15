f83b728e913703da97687165cf105181694be11c
nahomypolycar <112959201+nahomypolycar@users.noreply.github.com>
2022-12-15 20:24:01 +0100

    ajout du texte lorem pour l'étape 1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,18 @@ const Lorem = () => {
   return (
     <div className="Lorem">
       <h2>Lorem</h2>
-      <p>blablabkbihiuhiobiu</p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+        posuere rhoncus odio quis cursus. Vestibulum congue turpis eu tellus
+        maximus, nec consequat est lacinia. Nam pulvinar eleifend risus et
+        placerat. Suspendisse odio erat, rutrum nec viverra a, consectetur a
+        quam. Vestibulum erat nisi, tincidunt et neque et, laoreet blandit
+        justo. Phasellus aliquet orci odio, blandit pellentesque ipsum facilisis
+        ac. In ornare nunc ex, sodales pretium neque scelerisque id. Nulla
+        congue dui quis nisl accumsan scelerisque. Etiam viverra vel lectus
+        euismod facilisis. Aenean dictum urna urna, a blandit mauris volutpat
+        vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </p>
     </div>
   );
 };
